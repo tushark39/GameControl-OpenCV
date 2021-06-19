e7ef44f238dca505411d30d1fe76e3da27d390d3
Tushar <tushark39@gmail.com>
2021-06-19 13:34:23 +0530

    Game Simulator Raw

--- HandGestureDetection.py
+++ HandGestureDetection.py
@@ -3,31 +3,24 @@ import mediapipe as mp
 
 class HandDetector():
     def __init__(self):
-        self.mode = False
-        self.maxHands = 2
-        self.minDetectionConfidence = 0.7
-        self.minTrackingConfidence = 0.5
-
+        # Initializers
         self.targetHand = mp.solutions.hands
-        self.hands = self.targetHand.Hands(self.mode, self.maxHands, self.minDetectionConfidence, self.minTrackingConfidence)
-
+        self.hands = self.targetHand.Hands(min_detection_confidence=0.7)
         self.mpLandmarkVisual = mp.solutions.drawing_utils
 
-    def detectLandMark(self, frame):
+    def getLandMarks(self, frame,landLoc=True,LandLocPoint=False):
+        landMarkList = []
         rgbFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         self.processedData = self.hands.process(rgbFrame)
-
-        if self.processedData.multi_hand_landmarks:
-            for connections in self.processedData.multi_hand_landmarks:
-                self.mpLandmarkVisual.draw_landmarks(frame, connections, self.targetHand.HAND_CONNECTIONS)
-        return frame
-
-    def getLandMarks(self, frame):
-        landMarkList = []
         if self.processedData.multi_hand_landmarks:
             selectedHand = self.processedData.multi_hand_landmarks[0]
             for index, val in enumerate(selectedHand.landmark):
                 frameHeight, frameWidth, framedimension = frame.shape
                 Xcord, Ycord = int(val.x * frameWidth), int(val.y * frameHeight)
                 landMarkList.append([index, Xcord, Ycord])
+                if LandLocPoint:
+                    cv2.circle(frame, (Xcord, Ycord), 10, (255, 0, 0), cv2.FILLED)
+                if landLoc:
+                    self.mpLandmarkVisual.draw_landmarks(frame, selectedHand, self.targetHand.HAND_CONNECTIONS)
+
         return landMarkList
