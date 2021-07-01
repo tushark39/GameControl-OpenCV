05d98d01d83aea9a0170fb943467a03070b661a6
Tushar <tushark39@gmail.com>
2021-07-01 13:19:34 +0530

    Directkey integration in Game

--- GameSimulator.py
+++ GameSimulator.py
@@ -1,13 +1,17 @@
 import cv2
 import HandGestureDetection as hd
 # import time
-import pyautogui
+import pydirectinput as pyautogui
+#import pyautogui
 import math
 import FindAxisDistance
+import pygame
 
+from directkeys import PressKey, W, A, S, D,ReleaseKey
 # time.sleep(2)
-
-cap = cv2.VideoCapture(0)
+pyautogui.PAUSE = 0
+pyautogui.FAILSAFE = False
+cap = cv2.VideoCapture(1)
 
 handDataCap = hd.HandDetector()
 Distance = FindAxisDistance.AxisCal()
@@ -16,29 +20,37 @@ while True:
     if ret:
         frame = cv2.flip(frame, 1)
         landMarkList = handDataCap.getLandMarks(frame)
-        if len(landMarkList) == 21:
-            pyautogui.press('w')
+        #if len(landMarkList) == 21:
+            #pyautogui.press('w')
         if len(landMarkList):
 
             # Left
             lengthL = Distance.findDistance(landMarkList, 4, 8)
             if lengthL < 28:
-                pyautogui.press('a')
-
+                PressKey(A)
+            elif lengthL >28:
+                ReleaseKey(A)
+            
             # Right
             lengthR = Distance.findDistance(landMarkList, 4,12)
             if lengthR < 28:
-                pyautogui.press('d')
+                PressKey(D)
+            elif lengthL >28:
+                ReleaseKey(D)
 
-            # Down
-            lengthD = Distance.findDistance(landMarkList, 4, 16)
-            if lengthD < 30:
-                pyautogui.press('s')
+#             # Down
+#             lengthD = Distance.findDistance(landMarkList, 4, 16)
+#             if lengthD < 30:
+#                 pyautogui.press('down')
+#                 # pygame.KEYDOWN
 
-            # Break
-            lengthBreak = Distance.findDistance(landMarkList, 20, 17)
-            if lengthBreak < 25:
-                break
+#             # Break
+            lengthBreak = Distance.findDistance(landMarkList, 12, 0)
+            if lengthBreak < 100:
+                PressKey(W)
+            elif lengthL >100:
+                ReleaseKey(W)
+# #break
 
         cv2.imshow('Simulation', frame)
         if cv2.waitKey(1) == 27:
